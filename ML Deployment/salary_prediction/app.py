d2b751172295f7a84881f1ea9bfec7537c0334be
pkumar25 <prakash.kumar25@mrcooper.com>
2020-12-23 11:24:34 +0530

    adding projects

--- ML Deployment/salary_prediction/app.py
+++ ML Deployment/salary_prediction/app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-#loading the model
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
@@ -25,5 +24,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port = 8080)
